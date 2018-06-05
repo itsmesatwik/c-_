4c5ab5c1d13484d776fe5b39b2abc304b7cebfa7
satwik <satwik.illini@gmail.com>
2018-06-04 22:18:48 -0500

    Preprocessor directives and macro functions

---
+++ templates/general.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // this is called  a preprocessor directive meaning it will run before the program starts
+#include <cmath>
+#define PI 3.14159 // defines PI as 3.14159
+
+#define AREA_CIRCLE(radius) (PI * (std::pow(radius, 2))) // this is called a macro function
