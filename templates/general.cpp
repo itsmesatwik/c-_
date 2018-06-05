09d0bd3a3b97fd007f135beb62f0e6d1e94572bb
satwik <satwik.illini@gmail.com>
2018-06-04 23:05:43 -0500

    template functions

--- templates/general.cpp
+++ templates/general.cpp
@@ -3,3 +3,17 @@
 #define PI 3.14159 // defines PI as 3.14159
 
 #define AREA_CIRCLE(radius) (PI * (std::pow(radius, 2))) // this is called a macro function
+
+
+// Template functions
+template<typename T>
+void TimesN(T val) {
+	std::cout << val * n << "\n"; // The operator must hold meaning for the object and must be overloaded for custom objects
+}
+
+
+template<typename T>
+T Max(T val, T val2) {
+	return (val < val2) ? val2 : val;
+}
+
