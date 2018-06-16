7036449c9cde56350b239ab6455a758df4f2f8b5
satwik <satwik.illini@gmail.com>
2018-06-16 13:12:44 -0500

    Preprocessor directive

--- templates/preprocessor_directives.h
+++ templates/preprocessor_directives.h
@@ -1,8 +1,12 @@
 // These are called preprocessor directives and 
 // run before everything else in the program
 // These are identified by their signatory "#" prefix
+
 #ifndef ANIMAL_H // This guards against including this header in multiple different programs
 #define ANIMAL_H // makes sure that we read the following lines only a single time
+
+#define PI 3.14158 // Defining macro constants
+#define AREA_CIRCLE(radius) (PI*(std::pow(radius, 2))) // Defining macro functions
 
 class Animal {
 public:
