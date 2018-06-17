6dbd8f2452e012420fbdbadee9a86e5850311b4a
satwik <satwik.illini@gmail.com>
2018-06-17 13:02:15 -0500

    Implemented template

--- templates/template_class.cpp
+++ templates/template_class.cpp
@@ -1,3 +1,15 @@
 #include "template_class.h"
 
+// When we templatize something we must use <Object Type> when calling the object
+// Example: TemplateObject<ObjectType> object1();   
 
+template<typename T, typename U>
+Person<T,U>::Person(T h, U w) {
+    height = h, weight = w;
+    numOfPeople++;
+}
+
+template<typename T, typename U>
+Person<T,U>::getData() {
+    std::cout<<height<<weight;
+}
